29e6bd007072e31bebb143a6055979a27a1c202d
mehakekarigar <mehakekarigar@gmail.com>
2025-07-17 15:38:04 +0530

    integrating offline app handling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,11 @@ import { LoadingInterceptor } from './interceptors/loading.service';
 import { IonicStorageModule } from '@ionic/storage-angular';
 // import { FormTabsComponent } from './components/form-tabs/form-tabs.component';
 import { ServerErrorInterceptor } from './interceptors/http-error.interceptor';
+import { OfflineOverlayComponent } from './components/offline-overlay/offline-overlay.component';
 
 
 @NgModule({
-  declarations: [AppComponent,SplashScreenComponent],
+  declarations: [AppComponent,SplashScreenComponent, OfflineOverlayComponent],
   imports: [BrowserModule, IonicModule.forRoot(),IonicStorageModule.forRoot(), AppRoutingModule,HttpClientModule,FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },  {
       provide: HTTP_INTERCEPTORS,
